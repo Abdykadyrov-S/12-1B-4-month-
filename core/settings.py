c1d347621c40ee3042609125c421f032286347bb
Simon070805 <abdykadyrovsyimyk0708>
2023-12-19 18:17:23 +0600

    dp

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # custom admin panel jazzmin
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
